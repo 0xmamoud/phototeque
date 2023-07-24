f3606e0a569e1b3725081e81f37e5f23e77dc510
KANE <mahamadoudaba.kane@gmail.com>
2023-07-24 12:18:12 +0200

    fin du projet

--- controllers/album.controller.js
+++ controllers/album.controller.js
@@ -2,16 +2,17 @@ const Album = require("../models/Album");
 const path = require("path");
 const fs = require("fs");
 const rimraf = require("rimraf");
+const catchAsync = require("../helpers/catchAsync");
 
-const albums = async (req, res) => {
+const albums = catchAsync(async (req, res) => {
   const albums = await Album.find();
   res.render("albums", {
     title: "Mes albums",
     albums,
   });
-};
+});
 
-const album = async (req, res) => {
+const album = catchAsync(async (req, res) => {
   try {
     const idAlbum = req.params.id;
     const album = await Album.findById(idAlbum);
@@ -24,9 +25,9 @@ const album = async (req, res) => {
     console.log(error);
     res.redirect("/404");
   }
-};
+});
 
-const addImage = async (req, res) => {
+const addImage = catchAsync(async (req, res) => {
   const idAlbum = req.params.id;
   const album = await Album.findById(idAlbum);
 
@@ -57,16 +58,16 @@ const addImage = async (req, res) => {
   await album.save();
 
   res.redirect(`/albums/${idAlbum}`);
-};
+});
 
-const createAlbumForm = (req, res) => {
+const createAlbumForm = catchAsync((req, res) => {
   res.render("new-album", {
     title: "Nouvel album",
     errors: req.flash("error"),
   });
-};
+});
 
-const createAlbum = async (req, res) => {
+const createAlbum = catchAsync(async (req, res) => {
   try {
     if (!req.body.albumTitle) {
       req.flash("error", "Le titre ne doit pas être vide");
@@ -81,9 +82,9 @@ const createAlbum = async (req, res) => {
     req.flash("error", "Erreur lors de la création de l'album");
     res.redirect("/albums/create");
   }
-};
+});
 
-const deleteAlbum = async (req, res) => {
+const deleteAlbum = catchAsync(async (req, res) => {
   const idAlbum = req.params.id;
   await Album.findByIdAndDelete(idAlbum);
 
@@ -92,9 +93,9 @@ const deleteAlbum = async (req, res) => {
   rimraf(albumPath, () => {
     res.redirect("/albums");
   });
-};
+});
 
-const deleteImage = async (req, res) => {
+const deleteImage = catchAsync(async (req, res) => {
   const idAlbum = req.params.id;
   const album = await Album.findById(idAlbum);
 
@@ -111,7 +112,7 @@ const deleteImage = async (req, res) => {
   const imagePath = path.join(__dirname, "../public/upload", idAlbum, image);
   fs.unlinkSync(imagePath);
   res.redirect(`/albums/${idAlbum}`);
-};
+});
 
 module.exports = {
   albums,
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
+require("dotenv").config();
 const flash = require("connect-flash");
 const fileUpload = require("express-fileupload");
 const albumRoutes = require("./routes/album.route");
 
 const app = express();
 const PORT = 3000;
+const secret = process.env.TOKEN;
 
 mongoose.connect("mongodb://127.0.0.1:27017/phototeque");
 
@@ -22,7 +24,7 @@ app.use(express.static("public"));
 app.set("trust proxy", 1); // trust first proxy
 app.use(
   session({
-    secret: "keyboard cat",
+    secret,
     resave: false,
     saveUninitialized: true,
   })
@@ -37,6 +39,12 @@ app.get("/", (req, res) => {
 
 app.use((req, res) => {
   res.status(404).send("Page non trouvée");
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500);
+  res.send("Erreur interne du serveur");
 });
 
 app.listen(PORT, () => {
